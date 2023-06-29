feff9f5aa64f268166f1cc6b6c2fa66701c3c453
pavel.zybenkov <pavel.zybenkov@antilatency.com>
2023-06-29 19:06:04 +0300

    g/s params replaced with ENTRY_POINT env var

--- templates/StaticSharpProjectTemplate/Program.cs
+++ templates/StaticSharpProjectTemplate/Program.cs
@@ -8,21 +8,35 @@ namespace StaticSharpProjectTemplate
     {
         private static async Task Main(string[] args)
         {
-            switch (args.FirstOrDefault())
+            var entryPointName = Environment.GetEnvironmentVariable("ENTRY_POINT");
+            if (entryPointName == null)
             {
-                case "s":
-                    await Server();
-                    break;
-                case "g":
-                    await Generator();
-                    break;
-                default:
-                    Console.WriteLine("Add command line parameter: \"s\" - server mode, \"g\" - generator mode");
-                    Console.WriteLine(args.FirstOrDefault());
-                    break;
+                Console.WriteLine("EnvironmentVariable 'ENTRY_POINT' not found.");
+                return;
             }
 
-            Console.WriteLine(args.Length);
+            var entryPoint = typeof(Program).GetMethod(entryPointName,
+                System.Reflection.BindingFlags.Public
+                | System.Reflection.BindingFlags.Static
+                | System.Reflection.BindingFlags.FlattenHierarchy);
+
+            if (entryPoint == null)
+            {
+                Console.WriteLine($"entryPoint {entryPointName} not found.");
+                return;
+            }
+
+            if (entryPoint.ReturnType == typeof(Task))
+            {
+                var task = entryPoint.Invoke(null, null) as Task;
+                if (task != null)
+                {
+                    await task;
+                }
+            } else
+            {
+                entryPoint.Invoke(null, null);
+            }
         }
 
         public static async Task Server()
